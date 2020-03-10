d44e9f9f80cd4d9a5739b658d76b221f91941b37
debuginn <debuginn@icloud.com>
2020-03-10 15:40:00 +0800

    go web template demo

---
+++ goweb_template/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+func sayHello(w http.ResponseWriter, r *http.Request) {
+	// 2、解析模版
+	t, err := template.ParseFiles("./hello.tmpl")
+	if err != nil {
+		fmt.Println("template import faild, err: %v", err)
+		return
+	}
+	// 3、渲染模版
+	err = t.Execute(w, "demo")
+	if err != nil {
+		fmt.Println("template error, err: %v", err)
+	}
+}
+
+func main() {
+	// 1、定义路由及端口
+	http.HandleFunc("/", sayHello)
+	err := http.ListenAndServe(":9000", nil)
+	if err != nil {
+		fmt.Println("HTTP Server Start faild, err: %v", err)
+		return
+	}
+}
