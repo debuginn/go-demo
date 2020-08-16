95c4205bbcf5434edf253b0cd1a49d512f211c5a
mengxianhui <mengxianhui@xiaomi.com>
2020-08-16 13:20:26 +0800

    add study file

--- studygolang/sync/main.go
+++ studygolang/sync/main.go
@@ -1,5 +1,56 @@
 package main
 
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+var counter int
+
 func main() {
-	$END$
+	beginTime := time.Now()
+	//fmt.Printf("----------- sync wait group -------------\n")
+	//syncDemo()
+	fmt.Printf("----------- sync wait group lock -------------\n")
+	syncDemoLock()
+
+	endTime := time.Since(beginTime)
+	fmt.Printf("time spend: %s\n", endTime)
+}
+
+func syncDemo() {
+	var wg sync.WaitGroup
+
+	for i := 0; i < 1000; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+
+			counter++
+		}()
+	}
+
+	wg.Wait()
+
+	println(counter)
+}
+
+func syncDemoLock() {
+	var wg sync.WaitGroup
+	var l sync.Mutex
+
+	for i := 0; i < 10000; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			l.Lock()
+			counter++
+			l.Unlock()
+		}()
+	}
+
+	wg.Wait()
+
+	println(counter)
 }
--- studygolang/trylock/main.go
+++ studygolang/trylock/main.go
@@ -1,5 +1,60 @@
 package main
 
+import (
+	"fmt"
+	"sync"
+)
+
+// try Lock
+type Lock struct {
+	c chan struct{}
+}
+
+func newLock() Lock {
+	var l Lock
+	l.c = make(chan struct{}, 1)
+	l.c <- struct{}{}
+
+	return l
+}
+
+func (l Lock) Lock() bool {
+	lockRes := false
+
+	select {
+	case <-l.c:
+		lockRes = true
+	default:
+	}
+
+	return lockRes
+}
+
+func (l Lock) Unlock() {
+	l.c <- struct{}{}
+}
+
+var counter int
+
 func main() {
-	$END$
+	var l = newLock()
+	var wg sync.WaitGroup
+
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+
+		go func() {
+			defer wg.Done()
+			if !l.Lock() {
+				println("lock failed")
+				return
+			}
+
+			counter++
+			fmt.Printf("current counter: %d\n", counter)
+			l.Unlock()
+		}()
+	}
+
+	wg.Wait()
 }
